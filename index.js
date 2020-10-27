e7633b6f1124fab056fd26f141ec73c0a5a6e3e6
Chintan Shah <chintan@wohlig.com>
2020-10-27 19:15:49 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { isObject } = require("lodash");
 var _ = require("lodash");
+var fs = require("fs");
 var oldFile = require("./data/2954260.json");
 
 var newFile = _.map(oldFile, function (obj) {
   var retObj = {
     d: obj.createdAt,
-    b: { p: obj.rateBack.price, s: obj.rateBack.size },
-    l: { p: obj.rateLay.price, s: obj.rateLay.size },
   };
+  if (obj.rateBack) {
+    retObj.b = { p: obj.rateBack.price, s: obj.rateBack.size };
+  }
+  if (obj.rateLay) {
+    retObj.l = { p: obj.rateLay.price, s: obj.rateLay.size };
+  }
+  return retObj;
 });
 
 let data = JSON.stringify(newFile);
