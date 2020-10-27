c378261d79afb2ec066f2c3e4a68c0cf00e12e87
Chintan Shah <chintan@wohlig.com>
2020-10-27 19:13:06 +0530

    changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var _ = require("lodash")
-var oldFile = require("./data/2954260.json")
+const { isObject } = require("lodash");
+var _ = require("lodash");
+var oldFile = require("./data/2954260.json");
+
+var newFile = _.map(oldFile, function (obj) {
+  var retObj = {
+    d: obj.createdAt,
+    b: { p: obj.rateBack.price, s: obj.rateBack.size },
+    l: { p: obj.rateLay.price, s: obj.rateLay.size },
+  };
+});
+
+let data = JSON.stringify(newFile);
+fs.writeFileSync("compressed/2954260.json", data);
