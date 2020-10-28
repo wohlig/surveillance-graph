ba02d0b25d8f5df03fb7571d152fc5cfb0eae98a
Chintan Shah <chintan@wohlig.com>
2020-10-28 14:28:00 +0530

    changes

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ var moment = require("moment");
 var _ = require("lodash");
 var fs = require("fs");
 var oldFile = require("./data/2954260.json");
-
+var oldBack = 0;
+var oldLay = 0;
 var newFile = _.map(oldFile, function (obj) {
   var retObj = {
     d: moment(obj.createdAt).unix(),
   };
   if (obj.rateBack) {
-    retObj.b = { p: obj.rateBack.price, s: obj.rateBack.size };
+    retObj.b = obj.rateBack.price;
+    oldBack = retObj.b;
+  } else {
+    retObj.b = oldBack;
   }
   if (obj.rateLay) {
-    retObj.l = { p: obj.rateLay.price, s: obj.rateLay.size };
+    retObj.l = obj.rateLay.price;
+    oldLay = retObj.l;
+  } else {
+    retObj.l = oldLay;
   }
   return retObj;
 });
